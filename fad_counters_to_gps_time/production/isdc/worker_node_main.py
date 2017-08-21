0bedec9a78178c2adc6bf3b897aa60a0e9aee74f
Sebastian Mueller <sebmuell@phys.ethz.ch>
2017-08-21 18:27:04 +0200

    always show progres

--- fad_counters_to_gps_time/production/isdc/worker_node_main.py
+++ fad_counters_to_gps_time/production/isdc/worker_node_main.py
@@ -1,15 +1,15 @@
 """
-Usage: fact_fad_counter_extraction -i=INPUT_RUN_PATH -o=OUT_PATH
+Usage: 
+    fact_counter_extraction -i=PATH -o=PATH
 
 Options:
-    -i --input_run_path=INPUT_RUN_PATH
-    -o --out_path=OUT_PATH
+    -i, --input_run_path=PATH
+    -o, --out_path=PATH
 
 Extracts the FAD counters of each event from a FACT raw file and writes them
 into a separate output file.
 """
 import docopt
-import os
 import shutil 
 import tempfile
 from ...read_fad_counters import read_fad_counters
@@ -20,15 +20,17 @@ from os.path import split
 def main():
     try:
         arguments = docopt.docopt(__doc__)
-
         input_run_path = arguments['--input_run_path']
         out_path = arguments['--out_path']
-
+          
         with tempfile.TemporaryDirectory(prefix='fact_fad_counters') as tmp:
             input_run_base = split(input_run_path)[1]
             tmp_input_run_path = join(tmp, input_run_base)
             shutil.copy(input_run_path, tmp_input_run_path)
-            fad_counters = read_fad_counters(path=tmp_input_run_path)
+            fad_counters = read_fad_counters(
+                path=tmp_input_run_path, 
+                show_progress=True,
+            )
             fad_counters.to_hdf(out_path, 'all')
 
     except docopt.DocoptExit as e:
