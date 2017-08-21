8eb9c8daf651037c7e5393651dd99a9ae62bda1d
sebastian <sebmuell@phys.ethz.ch>
2017-08-21 15:00:20 +0200

    test executable worker node script

---
+++ fad_counters_to_gps_time/tests/test_production.py
@@ -0,0 +1,20 @@
+import numpy as np
+import fad_counters_to_gps_time as fad2gps
+import tempfile
+import os
+from os.path import join
+from os.path import exists
+import pkg_resources
+import glob
+
+
+def test_production_write_worker_script():
+    with tempfile.TemporaryDirectory(prefix='photon_stream_test_production') as tmp:
+        worker_script_path = join(tmp, 'worker.sh')
+        fad2gps.production.isdc.write_worker_node_script(
+        	path=worker_script_path,
+		    input_run_path='hans',
+		    output_fad_counter_path='peter',
+        )
+        assert os.path.exists(worker_script_path)
+        assert os.access(worker_script_path, os.X_OK)
