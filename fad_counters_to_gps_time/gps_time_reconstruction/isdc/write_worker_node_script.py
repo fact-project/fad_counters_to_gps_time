3a77c52de80ae819b0f9705c139143724f7e70d2
Dominik Neise <neised@phys.ethz.ch>
2017-08-22 17:14:15 +0200

    stuff

--- fad_counters_to_gps_time/gps_time_reconstruction/isdc/write_worker_node_script.py
+++ fad_counters_to_gps_time/gps_time_reconstruction/isdc/write_worker_node_script.py
@@ -1,18 +1,28 @@
-import os, stat
+import os
+import stat
+
 
 def write_worker_node_script(
     path,
-    input_run_path,
-    output_fad_counter_path
+    fad_counter_path,
+    gps_time_path,
+    models_path,
 ):
     """
-    Writes an executable bash script for a worker node to extract the fad 
+    Writes an executable bash script for a worker node to extract the fad
     counters from one fact raw run.
     """
-    sh = '#!/bin/bash\n'
-    sh+= 'source $HOME/.bashrc\n'
-    sh+= 'eval "fad_counter_extraction -i '+input_run_path+' -o '+output_fad_counter_path+'"\n'
-
+    sh = (
+        "#!/bin/bash\n"
+        "source $HOME/.bashrc\n"
+        "var = 'gps_time_reconstruction {fad_counter_path} {gps_time_path}"
+        " {models_path}'\n"
+        "echo $var\n"
+        "eval $var\n"
+        ).format(
+        fad_counter_path=fad_counter_path,
+        gps_time_path=gps_time_path,
+        models_path=models_path)
     with open(path, 'w') as fout:
         fout.write(sh)
 
