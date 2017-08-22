a7b8696bb6d7865f3d3a68007d90861d334c005d
Dominik Neise <neised@phys.ethz.ch>
2017-08-22 17:18:05 +0200

    adjust path

--- fad_counters_to_gps_time/gps_time_reconstruction/copy_readmes.py
+++ fad_counters_to_gps_time/gps_time_reconstruction/copy_readmes.py
@@ -1,9 +1,10 @@
 import pkg_resources
-import shutil 
+import shutil
+
 
 def copy_top_level_readme_to(path):
     readme_res_path = pkg_resources.resource_filename(
-        'fad_counters_to_gps_time', 
-        'production/resources/top_level_readme.md'
+        'fad_counters_to_gps_time',
+        'gps_time_reconstruction/resources/top_level_readme.md'
     )
     shutil.copy(readme_res_path, path)
