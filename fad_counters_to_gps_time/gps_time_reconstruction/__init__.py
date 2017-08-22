8cc1ada9fbbcd1de78473a0ee471c2c2cacee24a
Dominik Neise <neised@phys.ethz.ch>
2017-08-22 14:37:09 +0200

    start working on gps time reco worker

--- fad_counters_to_gps_time/gps_time_reconstruction/__init__.py
+++ fad_counters_to_gps_time/gps_time_reconstruction/__init__.py
@@ -1,4 +1,32 @@
+"""
+Usage:
+    gps_time_reconstruction <fad_counter_path> <gps_time_path> <meta_path>
+"""
+import docopt
 from . import isdc
 from . import ethz
-from .make_job_list import make_job_list
-from .worker_node_fad_counter_extraction_main import run_fad_counter_extraction
+
+import shutil
+
+
+def gps_time_reconstruction(path):
+    pass
+
+
+def main(fad_counter_path, gps_time_path, meta_path):
+    gps_time, meta = gps_time_reconstruction(fad_counter_path)
+
+    gps_time.to_hdf(gps_time_path+'.part', 'all')
+    shutil.move(gps_time_path+'.part', gps_time_path)
+
+    meta.to_hdf(meta_path+'.part', 'all')
+    shutil.move(meta_path+'.part', meta_path)
+
+
+if __name__ == '__main__':
+    args = docopt.docopt(__doc__)
+    main(
+        fad_counter_path=args['<fad_counter_path>'],
+        gps_time_path=args['<gps_time_path>'],
+        meta_path=args['<meta_path>'],
+        )
