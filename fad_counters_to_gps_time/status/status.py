d9fb4213336131dd3447cc1e853d58fa42cbc548
Dominik Neise <neised@phys.ethz.ch>
2017-08-23 22:36:59 +0200

    add a __main__ for continous updating

--- fad_counters_to_gps_time/status/status.py
+++ fad_counters_to_gps_time/status/status.py
@@ -1,10 +1,19 @@
+"""
+Usage:
+  fad_counter_status_update [options]
+
+Options:
+  --init              Initialize
+  -k, --keep-running  keep updating
+"""
 import pandas as pd
 import os
 import shutil
 from tqdm import tqdm
 from ..production.make_job_list import OBSERVATION_RUN_KEY
 from fact import credentials
-
+from docopt import docopt
+import sys
 
 def make_run_path(run, base_dir, suffix='_fad.h5'):
     night = '{:08d}'.format(run.fNight)
@@ -53,11 +62,41 @@ def update_known_runs(fad_dir, known_runs_file_name='known_runs.h5'):
     else:
         known_runs = latest_runinfo()
     known_runs = update_status_runinfo(fad_dir=fad_dir, runinfo=known_runs)
-    known_runs.to_hdf(known_runs_path+'.part', 'all')
-    shutil.move(known_runs_path+'.part', known_runs_path)
+    to_hdf(known_runs, known_runs_path)
+
+
+def to_hdf(df, path):
+    df.to_hdf(path+'.part', 'all')
+    shutil.move(path+'.part', path)
 
 
 def latest_runinfo():
     factdb = credentials.create_factdb_engine()
     print("Reading fresh RunInfo table, takes about 1min.")
     return pd.read_sql_table("RunInfo", factdb)
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    print(args)
+
+    fad_dir = os.path.dirname(os.path.realpath(__file__))
+    known_runs_path = os.path.join(fad_dir, 'known_runs.h5')
+
+    if not os.path.exists(known_runs_path):
+        if args['--init']:
+            runinfo = latest_runinfo()
+            runinfo['FadCounterNumEvents'] = 0
+            to_hdf(runinfo, known_runs_path)
+        else:
+            print(
+                'This folder does not (yet) contain a known_runs.h5,'
+                ' call with --init to initialize.')
+            sys.exit(0)
+
+    while True:
+        known_runs = pd.read_hdf(known_runs_path)
+        known_runs = update_status_runinfo(fad_dir, known_runs)
+        to_hdf(known_runs, known_runs_path)
+        if not args['--keep-running']:
+            break
+
