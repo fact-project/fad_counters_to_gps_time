37d59a9e05a9cbd6ccab58c5312af1bb9f0144fe
Dominik Neise <neised@phys.ethz.ch>
2017-08-23 07:24:52 +0200

    fix tabs-spaces issue, remove unneeded imports

--- fad_counters_to_gps_time/tests/test_production.py
+++ fad_counters_to_gps_time/tests/test_production.py
@@ -1,4 +1,3 @@
-import numpy as np
 import fad_counters_to_gps_time as fad2gps
 import photon_stream as ps
 import tempfile
@@ -6,15 +5,15 @@ import os
 from os.path import join
 from os.path import exists
 import pkg_resources
-import glob
+
 
 def test_production_write_worker_script():
     with tempfile.TemporaryDirectory(prefix='fad2gps_') as tmp:
         worker_script_path = join(tmp, 'worker.sh')
         fad2gps.production.isdc.write_worker_node_script(
-        	path=worker_script_path,
-		    input_run_path='hans',
-		    output_fad_counter_path='peter',
+            path=worker_script_path,
+            input_run_path='hans',
+            output_fad_counter_path='peter',
         )
         assert os.path.exists(worker_script_path)
         assert os.access(worker_script_path, os.X_OK)
@@ -24,7 +23,7 @@ def test_production_idsc_qsub_production():
     with tempfile.TemporaryDirectory(prefix='fad2gps_') as tmp:
 
         runinfo_path = pkg_resources.resource_filename(
-            'fad_counters_to_gps_time', 
+            'fad_counters_to_gps_time',
             'tests/resources/runinfo_2014Dec15_2015Jan15.msg')
 
         runinfo = ps.production.runinfo.read_runinfo_from_file(runinfo_path)
