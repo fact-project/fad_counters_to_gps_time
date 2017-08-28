4f0ae21631ab9efb72ced91971cb27d9fb4ee583
Dominik Neise <neised@phys.ethz.ch>
2017-08-28 11:30:44 +0200

    use `shutil.which` over shelling out to `which`

--- fad_counters_to_gps_time/gps_time_reconstruction/isdc/qsub.py
+++ fad_counters_to_gps_time/gps_time_reconstruction/isdc/qsub.py
@@ -1,10 +1,11 @@
 from tqdm import tqdm
 import os
 import os.path
-from ..make_job_list import make_job_list
 from .dummy_qsub import dummy_qsub
 import subprocess as sp
+from shutil import which
 from ..copy_readmes import copy_top_level_readme_to
+
 
 def qsub(jobs, queue='fact_medium', use_dummy_qsub=False):
 
@@ -15,13 +16,12 @@ def qsub(jobs, queue='fact_medium', use_dummy_qsub=False):
         os.makedirs(os.path.split(job['std_out_path'])[0], exist_ok=True)
         os.makedirs(os.path.split(job['std_err_path'])[0], exist_ok=True)
 
-        script_path = sp.check_output(['which', 'gps_time_reconstruction']).strip()
         cmd = [
             'qsub',
             '-q', queue,
             '-o', job['std_out_path'],
             '-e', job['std_err_path'],
-            script_path,
+            which('gps_time_reconstruction'),
             job['input_file_path'],
             job['gps_time_path'],
             job['models_path'],
