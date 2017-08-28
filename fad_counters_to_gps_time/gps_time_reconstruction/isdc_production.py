82180a3ccf5efc00367d9348779a3a1e9db4d32c
Dominik Neise <neised@phys.ethz.ch>
2017-08-28 13:36:05 +0200

    debug and add logging

--- fad_counters_to_gps_time/gps_time_reconstruction/isdc_production.py
+++ fad_counters_to_gps_time/gps_time_reconstruction/isdc_production.py
@@ -4,8 +4,9 @@ Usage:
     isdc_production [options]
 
 Options:
-    -o, --output DIR  output_directory(default:/gpfs0/fact/processing/gps_time)
-    -i, --input DIR   input directory(default:/gpfs0/fact/processing/fad_counters/fad)
+    -o, --output DIR  output_directory [default: /gpfs0/fact/processing/gps_time]
+    -i, --input DIR   input directory [default: /gpfs0/fact/processing/fad_counters/fad]
+    -f, --fraction F  fraction of possible jobs to submit [default: 1.0]
 """
 import os
 from os.path import abspath
@@ -21,6 +22,12 @@ from tqdm import tqdm
 from fact.path import TreePath
 from fact.credentials import create_factdb_engine
 from docopt import docopt
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(message)s'
+)
 
 OBSERVATION_RUN_KEY = 1
 
@@ -40,9 +47,7 @@ def make_job_list(
     only_a_fraction=1.0,
     tmp_dir_base_name='gps_time_reco_',
 ):
-    jobs = run_info[
-        run_info.fRunTypeKey == OBSERVATION_RUN_KEY
-        ].sample(frac=only_a_fraction).copy()
+    jobs = run_info.sample(frac=only_a_fraction).copy()
 
     path_generators = {
         'input_file_path': TreePath(base_dir=fad_counter_dir, suffix='_fad.h5'),
@@ -53,8 +58,8 @@ def make_job_list(
     }
 
     for job in jobs.itertuples():
-        for name, generator in path_generators:
-            jobs.set(job.Index, name, generator(job.fNight, job.fRunID))
+        for name, generator in path_generators.items():
+            jobs.set_value(job.Index, name, generator(job.fNight, job.fRunID))
 
     jobs['input_file_exists'] = jobs.input_file_path.apply(exists)
 
@@ -63,7 +68,7 @@ def make_job_list(
 
 def qsub(jobs, queue='fact_medium'):
 
-    for job in tqdm(jobs.itertuples()):
+    for job in tqdm(jobs.itertuples(), total=len(jobs)):
         os.makedirs(dirname(job.std_out_path), exist_ok=True)
         os.makedirs(dirname(job.std_err_path), exist_ok=True)
 
@@ -87,25 +92,38 @@ def qsub(jobs, queue='fact_medium'):
 
 
 def main():
-    args = docopt.docopt(__doc__)
+    args = docopt(__doc__)
+    args['--fraction'] = float(args['--fraction'])
+    logging.info(str(args))
+
     out_dir = abspath(args['--output'])
     fad_counter_dir = abspath(args['--input'])
 
     os.makedirs(out_dir, exist_ok=True)
     copy_top_level_readme_to(join(out_dir, 'README.md'))
 
+    logging.info('downloading list of observation runs ... ')
     runinfo = pd.read_sql(
         '''
-        SELECT fNight, fRunID, fRunTypeKey from RunInfo
-        ''',
+        SELECT 
+            fNight, fRunID
+        FROM 
+            RunInfo
+        WHERE
+            fRunTypeKey={0}
+        '''.format(OBSERVATION_RUN_KEY),
         create_factdb_engine()
     )
+    logging.info('{0} observation runs found in RunInfo DB'.format(len(runinfo)))
 
     jobs = make_job_list(
         out_dir,
         fad_counter_dir,
         runinfo
     )
+    logging.info('for {0} of the {1} observation runs, the input file exists'.format(
+        len(runinfo),
+        len(jobs)))
 
     qsub(jobs)
 
