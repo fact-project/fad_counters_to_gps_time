fd52fea41fa4f71b181f092700bf05a25c960c91
Dominik Neise <neised@phys.ethz.ch>
2017-08-29 18:15:34 +0200

    rename console scripts

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='fad_counters_to_gps_time',
-    version='0.0.0',
+    version='0.0.1',
     description='process the GPS time for each FACT event',
     url='https://github.com/fact-project/',
     author='Dominik, Neise, Sebastian Achim Mueller',
@@ -10,11 +10,9 @@ setup(
     license='MIT',
     packages=[
         'fad_counters_to_gps_time',
-        'fad_counters_to_gps_time.fad_counter_production',
     ],
     package_data={'photon_stream': [
             'tests/resources/*',
-            'gps_time_reconstruction/README.md',
         ]
     },
     install_requires=[
@@ -27,23 +25,13 @@ setup(
         'pandas',
         'tqdm',
         'photon_stream',
+        'scoop',
     ],
     zip_safe=False,
     entry_points={'console_scripts': [
-        ('fad_counter_extraction = ' +
-            'fad_counters_to_gps_time.' +
-            'fad_counter_production.__init__:main'),
-        ('gps_time_reconstruction = ' +
-            'fad_counters_to_gps_time.' +
-            'gps_time_reconstruction.__init__:main'),
-        ('fad_counter_status_update = ' +
-            'fad_counters_to_gps_time.' +
-            'status.status:main'),
-        ('produce_gps_time_at_isdc = ' +
-            'fad_counters_to_gps_time.' +
-            'gps_time_reconstruction.isdc_production:main'),
-        ('produce_fad_counter_at_isdc = ' +
-            'fad_counters_to_gps_time.' +
-            'fad_counter_production.isdc_production:main'),
+        'single_fad_counter_extraction = fad_counters_to_gps_time.fad_counter_readout:main',
+        'single_gps_time_reco = fad_counters_to_gps_time.gps_time_reconstruction:main',
+        'produce_fad_counters = fad_counters_to_gps_time.fad_counter_production:main',
+        'produce_gps_time = fad_counters_to_gps_time.gps_time_production:main',
     ]},
 )
