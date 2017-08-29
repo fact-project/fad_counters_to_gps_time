d494dc9937c4ce0748630f7c43f8edb7f7b36206
Dominik Neise <neised@phys.ethz.ch>
2017-08-29 19:00:17 +0200

    method renamed ... test forgotten .. d'uh

--- fad_counters_to_gps_time/tests/test_time_reconstruction.py
+++ fad_counters_to_gps_time/tests/test_time_reconstruction.py
@@ -19,7 +19,7 @@ def test_make_counter_increasing_do_nothing_when_counter_is_good_already():
     df = tr.get_gps(df)
     # this counter happens to be okay.
     counter_1 = df['Counter_0']
-    counter_2 = tr.make_counter_strictly_monotonic_increasing(counter_1)
+    counter_2 = tr.make_counter_strictly_increasing(counter_1)
 
     assert (counter_2 == counter_1).all()
 
@@ -40,7 +40,7 @@ def test_make_counter_increasing_2():
     assert w[0] == 143
 
     # repair the overflow ...
-    counter_2 = tr.make_counter_strictly_monotonic_increasing(counter_1)
+    counter_2 = tr.make_counter_strictly_increasing(counter_1)
 
     # and make sure the new counter is really strictly increasing
     w = np.where(counter_2.astype(float).diff()[1:] <= 0)[0]
