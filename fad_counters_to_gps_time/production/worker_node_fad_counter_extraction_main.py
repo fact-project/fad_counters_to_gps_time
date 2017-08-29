5af7f74fa609c201b8f0dcb434aaf1450a8adda3
Dominik Neise <neised@phys.ethz.ch>
2017-08-29 16:04:56 +0200

    allow show progress switch

--- fad_counters_to_gps_time/production/worker_node_fad_counter_extraction_main.py
+++ fad_counters_to_gps_time/production/worker_node_fad_counter_extraction_main.py
@@ -1,23 +1,23 @@
 """
 Usage:
-    fact_counter_extraction -i=PATH -o=PATH
+    fact_counter_extraction <input_path> <output_path> [options]
 
 Options:
-    -i, --in_path=PATH       Path to a FACT raw fits run file.
-    -o, --out_path=PATH      Path of the output run with only the fad counters.
+    --show_progress    show a progress bar
 
 Extracts the FAD counters of each event from a FACT raw file and writes them
 into a separate output file.
 """
 import docopt
 import shutil
+from os.path import abspath
 from ..read_fad_counters import read_fad_counters
 
 
-def run_fad_counter_extraction(in_path, out_path):
+def run_fad_counter_extraction(in_path, out_path, show_progress):
     fad_counters = read_fad_counters(
         path=in_path,
-        show_progress=False,
+        show_progress=show_progress,
     )
     fad_counters.to_hdf(out_path+'.part', 'all')
     shutil.move(out_path+'.part', out_path)
@@ -25,10 +25,12 @@ def run_fad_counter_extraction(in_path, out_path):
 
 def main():
     try:
-        arguments = docopt.docopt(__doc__)
-        in_path = arguments['--in_path']
-        out_path = arguments['--out_path']
-        run_fad_counter_extraction(in_path=in_path, out_path=out_path)
+        args = docopt.docopt(__doc__)
+        run_fad_counter_extraction(
+            in_path=abspath(args['<input_path>']),
+            out_path=abspath(args['<output_path>']),
+            show_progress=args['--show_progress'],
+        )
 
     except docopt.DocoptExit as e:
         print(e)
