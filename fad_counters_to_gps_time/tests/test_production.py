b117d1bbd75b88875b01ab14392c8de143fc96bc
Dominik Neise <neised@phys.ethz.ch>
2017-08-29 19:01:31 +0200

    no workernode script is written anymore .. so no test needed

--- fad_counters_to_gps_time/tests/test_production.py
+++ fad_counters_to_gps_time/tests/test_production.py
@@ -7,18 +7,6 @@ from os.path import exists
 import pkg_resources
 
 
-def test_production_write_worker_script():
-    with tempfile.TemporaryDirectory(prefix='fad2gps_') as tmp:
-        worker_script_path = join(tmp, 'worker.sh')
-        fad2gps.production.isdc.write_worker_node_script(
-            path=worker_script_path,
-            input_run_path='hans',
-            output_fad_counter_path='peter',
-        )
-        assert os.path.exists(worker_script_path)
-        assert os.access(worker_script_path, os.X_OK)
-
-
 def test_production_idsc_qsub_production():
     with tempfile.TemporaryDirectory(prefix='fad2gps_') as tmp:
 
