aebe8535793b91bb8eed4f9488694fd03725cc19
Dominik Neise <neised@phys.ethz.ch>
2017-08-29 17:31:59 +0200

    delete

--- fad_counters_to_gps_time/production/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import isdc
-from . import ethz
-from .make_job_list import make_job_list
-from .worker_node_fad_counter_extraction_main import run_fad_counter_extraction
--- fad_counters_to_gps_time/production/isdc/__init__.py
+++
@@ -1,2 +0,0 @@
-from .write_worker_node_script import write_worker_node_script
-from .qsub import qsub
