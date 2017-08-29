bf4e1a476c3bfbfe9379b496806878f26a9e8103
Dominik Neise <neised@phys.ethz.ch>
2017-08-29 15:42:03 +0200

    sort according to interesting order

--- fad_counters_to_gps_time/gps_time_reconstruction/__init__.py
+++ fad_counters_to_gps_time/gps_time_reconstruction/__init__.py
@@ -3,6 +3,8 @@
 Usage:
     gps_time_reconstruction <fad_counter_path> <gps_time_path> <models_path>
 """
+from os.path import abspath
+from os.path import dirname
 import docopt
 import pandas as pd
 import numpy as np
@@ -24,55 +26,31 @@ class TooFewGpsEvents(Exception):
     pass
 
 
-def get_gps(df):
-    return df[df.Trigger.isin([trigger.EXT1, trigger.EXT2])]
+def main():
+    args = docopt.docopt(__doc__)
+    write_gps_time_reconstruction(
+        fad_counter_path=args['<fad_counter_path>'],
+        gps_time_path=args['<gps_time_path>'],
+        models_path=args['<models_path>'],
+        )
 
 
-def make_counter_strictly_increasing(counter):
-    counter = counter.astype(np.int64)
-    w = np.where(counter.diff() < 0)[0]
-    if not w:
-        return counter
-    else:
-        v = counter.values
-        v[w[0]:] += COUNTER_MAX
-        counter = pd.Series(index=counter.index, data=v)
-        return counter
+def write_gps_time_reconstruction(
+        fad_counter_path,
+        gps_time_path,
+        models_path
+):
+    gps_time, models = gps_time_reconstruction(fad_counter_path)
 
+    _dir = os.path.split(os.path.abspath(gps_time_path))[0]
+    os.makedirs(_dir, exist_ok=True)
+    gps_time.to_hdf(gps_time_path+'.part', 'all')
+    shutil.move(gps_time_path+'.part', gps_time_path)
 
-def more_precise_linear_fit(X, Y):
-    fit = np.polyfit(X - X[0], Y - Y[0], deg=1)
-    fit = (fit[0], (fit[1] + Y[0] - fit[0]*X[0]))
-    return fit
-
-
-def train_models(df):
-    fits = []
-    for board_id in range(40):
-        X = df['Counter_{0}'.format(board_id)].values
-        Y = df.time_rounded
-
-        fit = more_precise_linear_fit(X, Y)
-        residuals = Y - np.polyval(fit, X)
-        _chi2 = (residuals**2).sum() / SQUARE_TIME_ERROR_OF_COUNTER
-        p_value = chi2.sf(_chi2, len(df) - 2)
-        slope, intercept = fit
-        fits.append({
-            'board_id': board_id,
-            'slope': np.float128(slope),
-            'intercept': np.float128(intercept),
-            'p_value': p_value,
-            'chi2': _chi2,
-            })
-    return pd.DataFrame(fits)
-
-
-def apply_models(models, df):
-    prediction = np.zeros((len(models), len(df)), dtype=np.float128)
-    for m in models.itertuples():
-        counter = df['Counter_{0}'.format(m.board_id)]
-        prediction[m.board_id, :] = m.intercept + m.slope * counter
-    return prediction.mean(axis=0)
+    _dir = os.path.split(os.path.abspath(models_path))[0]
+    os.makedirs(_dir, exist_ok=True)
+    models.to_hdf(models_path+'.part', 'all')
+    shutil.move(models_path+'.part', models_path)
 
 
 def gps_time_reconstruction(path):
@@ -107,31 +85,55 @@ def gps_time_reconstruction(path):
     return out_df, models
 
 
-def write_gps_time_reconstruction(
-        fad_counter_path,
-        gps_time_path,
-        models_path
-):
-    gps_time, models = gps_time_reconstruction(fad_counter_path)
-
-    _dir = os.path.split(os.path.abspath(gps_time_path))[0]
-    os.makedirs(_dir, exist_ok=True)
-    gps_time.to_hdf(gps_time_path+'.part', 'all')
-    shutil.move(gps_time_path+'.part', gps_time_path)
-
-    _dir = os.path.split(os.path.abspath(models_path))[0]
-    os.makedirs(_dir, exist_ok=True)
-    models.to_hdf(models_path+'.part', 'all')
-    shutil.move(models_path+'.part', models_path)
+def make_counter_strictly_increasing(counter):
+    counter = counter.astype(np.int64)
+    w = np.where(counter.diff() < 0)[0]
+    if not w:
+        return counter
+    else:
+        v = counter.values
+        v[w[0]:] += COUNTER_MAX
+        counter = pd.Series(index=counter.index, data=v)
+        return counter
 
 
-def main():
-    args = docopt.docopt(__doc__)
-    write_gps_time_reconstruction(
-        fad_counter_path=args['<fad_counter_path>'],
-        gps_time_path=args['<gps_time_path>'],
-        models_path=args['<models_path>'],
-        )
+def get_gps(df):
+    return df[df.Trigger.isin([trigger.EXT1, trigger.EXT2])]
+
+
+def train_models(df):
+    fits = []
+    for board_id in range(40):
+        X = df['Counter_{0}'.format(board_id)].values
+        Y = df.time_rounded
+
+        fit = more_precise_linear_fit(X, Y)
+        residuals = Y - np.polyval(fit, X)
+        _chi2 = (residuals**2).sum() / SQUARE_TIME_ERROR_OF_COUNTER
+        p_value = chi2.sf(_chi2, len(df) - 2)
+        slope, intercept = fit
+        fits.append({
+            'board_id': board_id,
+            'slope': np.float128(slope),
+            'intercept': np.float128(intercept),
+            'p_value': p_value,
+            'chi2': _chi2,
+            })
+    return pd.DataFrame(fits)
+
+
+def more_precise_linear_fit(X, Y):
+    fit = np.polyfit(X - X[0], Y - Y[0], deg=1)
+    fit = (fit[0], (fit[1] + Y[0] - fit[0]*X[0]))
+    return fit
+
+
+def apply_models(models, df):
+    prediction = np.zeros((len(models), len(df)), dtype=np.float128)
+    for m in models.itertuples():
+        counter = df['Counter_{0}'.format(m.board_id)]
+        prediction[m.board_id, :] = m.intercept + m.slope * counter
+    return prediction.mean(axis=0)
 
 
 if __name__ == '__main__':
