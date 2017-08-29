005283acba4a5404317bb7e2377a0cc55541e969
Dominik Neise <neised@phys.ethz.ch>
2017-08-29 17:28:16 +0200

    scoop production

--- fad_counters_to_gps_time/fad_counter_production/scoop_production.py
+++ fad_counters_to_gps_time/fad_counter_production/scoop_production.py
@@ -1,4 +1,13 @@
+#!/bin/env python
 """
+Usage:
+    scoop_production [options]
+
+Options:
+    -o, --output DIR  output_directory [default: /data/fact/fad_counter_new]
+    -i, --input DIR   input directory [default: /fact/raw]
+    --init            initialize the runinfo storage for this processing
+
 Usage: scoop_produce_phs [options]
 
 Options:
@@ -8,46 +17,219 @@ Options:
     --fact_raw_dir=DIR          [default: /data/fact_data]
     --tmp_dir_base_name=BASE    [default: fact_fad_counter_]
 """
-import docopt
 import scoop
 import os
-import fad_counters_to_gps_time as fad2gps
+import sys
+from os import remove
+from os.path import abspath
+from os.path import join
+from os.path import exists
+from os.path import dirname
+from datetime import datetime
+import shutil
+import pkg_resources
+from shutil import which
+import subprocess as sp
 import pandas as pd
+from tqdm import tqdm
+from fact.path import TreePath
+from fact.credentials import create_factdb_engine
+from docopt import docopt
+import logging
+import numpy as np
+from . import run_fad_counter_extraction
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(message)s'
+)
 
-def run_fad_extraction_job(job):
-    os.makedirs(job['fad_yyyy_mm_nn_dir'], exist_ok=True)
-    fad2gps.production.run_fad_counter_extraction(
-        in_path=job['raw_path'],
-        out_path=job['fad_path']
-    )
-    return 0
+OBSERVATION_RUN_KEY = 1
+SQL_QUERY = '''
+SELECT
+    fNight, fRunID, fNumEvents
+FROM
+    RunInfo
+WHERE
+    fRunTypeKey={0}
+'''.format(OBSERVATION_RUN_KEY)
 
 
 def main():
-    try:
-        arguments = docopt.docopt(__doc__)
+    args = docopt(__doc__)
+    out_dir = abspath(args['--output'])
+    input_dir = abspath(args['--input'])
+    runstatus_path = join(out_dir, 'runinfo.csv')
+    os.makedirs(out_dir, exist_ok=True)
+    copy_top_level_readme_to(join(out_dir, 'README.md'))
+    path_gens = init_path_generators(input_dir, out_dir)
 
-        runinfo = pd.read_msgpack(arguments['--run_info_path'])
+    if args['--init']:
+        if exists(runstatus_path):
+            logging.error((
+                'runstatus.csv already exists in out_dir.\n' +
+                '%s\n' +
+                'Running with --init would overwrite that. ' +
+                'Please remove it yourself.'
+                ).format(out_dir))
+            sys.exit(-1)
+        runstatus = initialize_runstatus()
 
-        job_structure = fad2gps.production.make_job_list(
-            out_dir=arguments['--out_dir'],
-            run_info=runinfo,
-            only_a_fraction=float(arguments['--only_a_fraction']),
-            fact_raw_dir=arguments['--fact_raw_dir'],
-            tmp_dir_base_name=arguments['--tmp_dir_base_name'],
+    if not exists(runstatus_path):
+        logging.error(
+            'runstatus.csv file does not exist. Call with --init first')
+        sys.exit(-1)
+
+    runstatus = update_runstatus(runstatus_path)
+
+    # Note: these modify runstatus in place
+    check_for_input_files(runstatus, path_gens['input_file_path'])
+    check_for_output_files(runstatus, path_gens['output_file_path'])
+    check_length_of_output(runstatus, path_gens['output_file_path'])
+
+    runs_not_yet_submitted = runstatus[
+        runstatus.input_file_exists &
+        (~runstatus.output_file_exists) &
+        np.isnat(runstatus.submitted_at)
+    ]
+
+    for job in runs_not_yet_submitted.itertuples():
+        runstatus.set_value(
+            job.Index,
+            'submitted_at',
+            datetime.utcnow()
         )
 
-        jobs = job_structure['jobs']
-        out_dirs = job_structure['directory_structure']
-        os.makedirs(out_dirs['out_dir'], exist_ok=True)
-        os.makedirs(out_dirs['fad_dir'], exist_ok=True)
-        fad2gps.production.copy_readmes.copy_top_level_readme_to(os.path.join(out_dirs['out_dir'], 'README.md'))
+    scoop.futures.map(
+        run_fad_counter_extraction,
+        [
+            {
+                'in_path': path_gens['input_file_path'](job),
+                'out_path': path_gens['output_file_path'](job),
+            }
+            for job in runs_not_yet_submitted.itertuples()
+        ]
+    )
 
-        job_return_codes = list(scoop.futures.map(run_fad_extraction_job, jobs))
+    runstatus.to_csv(runstatus_path, 'all')
 
-    except docopt.DocoptExit as e:
-        print(e)
+
+def init_path_generators(input_dir, out_dir):
+    return {
+        'input_file_path': TreePath(base_dir=input_dir, suffix='.fits.fz'),
+        'std_out_path': TreePath(join(out_dir, 'std'), '.o'),
+        'std_err_path': TreePath(join(out_dir, 'std'), '.e'),
+        'output_file_path':
+            TreePath(join(out_dir, 'gps_time'), '_fad.h5'),
+    }
+
+
+def copy_top_level_readme_to(path):
+    this_sub_package_name = os.path.split(
+        os.path.dirname(os.path.realpath(__file__)))[-1]
+    readme_res_path = pkg_resources.resource_filename(
+        'fad_counters_to_gps_time',
+        this_sub_package_name+'/README.md'
+    )
+    shutil.copy(readme_res_path, path)
+
+
+def initialize_runstatus():
+    runinfo = pd.read_sql(SQL_QUERY, create_factdb_engine())
+    runinfo['input_file_exists'] = False
+    runinfo['output_file_exists'] = False
+    runinfo['length_of_output'] = np.nan
+    runinfo['submitted_at'] = pd.Timestamp('nat')
+
+    return runinfo
+
+
+def update_runstatus(path):
+    logging.info('downloading list of observation runs ... ')
+    runinfo = pd.read_sql(SQL_QUERY, create_factdb_engine())
+    runinfo = runinfo.merge(
+        pd.read_csv(path),
+        on=list(runinfo.columns),
+        how='outer',
+    )
+    runinfo.input_file_exists.fillna(False, inplace=True)
+    runinfo.output_file_exists.fillna(False, inplace=True)
+    return runinfo
+
+
+def check_for_input_files(runinfo, path_generator):
+    no_input_files = runinfo[~runinfo.input_file_exists]
+    for job in tqdm(
+            no_input_files.itertuples(),
+            desc='check_for_input_file:',
+            total=len(no_input_files)
+    ):
+        runinfo.set_value(
+            job.Index,
+            'input_file_exists',
+            exists(path_generator(job))
+        )
+
+
+def check_for_output_files(runinfo, path_generator):
+    no_output_files = runinfo[~runinfo.output_file_exists]
+    for job in tqdm(
+            no_output_files.itertuples(),
+            desc='check_for_ouput_file:',
+            total=len(no_output_files)
+    ):
+        runinfo.set_value(
+            job.Index,
+            'output_file_exists',
+            exists(path_generator(job))
+        )
+
+
+def check_length_of_output(runinfo, path_generator):
+    to_be_checked = runinfo[
+        np.isnan(runinfo.length_of_output) &
+        runinfo.output_file_exists
+    ]
+    for job in tqdm(
+            to_be_checked.itertuples(),
+            desc='check_output_length:',
+            total=len(to_be_checked)
+    ):
+        runinfo.set_value(
+            job.Index,
+            'length_of_output',
+            float(len(pd.read_hdf(path_generator(job))))
+        )
+
+
+def qsub(job, path_gens, queue='fact_medium'):
+
+    for p in [
+        path_gens['std_out_path'](job),
+        path_gens['std_err_path'](job)
+    ]:
+        if exists(p):
+            remove(p)
+        else:
+            os.makedirs(dirname(p), exist_ok=True)
+
+    cmd = [
+        'qsub',
+        '-q', queue,
+        '-o', path_gens['std_out_path'](job),
+        '-e', path_gens['std_err_path'](job),
+        which('fad_counter_extraction'),
+        path_gens['input_file_path'](job),
+        path_gens['output_file_path'](job),
+    ]
+
+    try:
+        sp.check_output(cmd, stderr=sp.STDOUT)
+    except sp.CalledProcessError as e:
+        print('returncode', e.returncode)
+        print('output', e.output)
+        raise
+
 
 if __name__ == '__main__':
     main()
