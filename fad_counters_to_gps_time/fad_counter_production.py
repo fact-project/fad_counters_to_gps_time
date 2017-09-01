7180385902352e61bb092fa918fa1397698cd61a
Dominik Neise <neised@phys.ethz.ch>
2017-09-01 16:00:55 +0200

    use manure

--- fad_counters_to_gps_time/fad_counter_production.py
+++ fad_counters_to_gps_time/fad_counter_production.py
@@ -1,32 +1,16 @@
 #!/bin/env python
-"""
-Usage:
-    isdc_production [options]
 
-Options:
-    -o, --output DIR  output_directory [default: /gpfs0/fact/processing/public/fad_counter]
-    -i, --input DIR   input directory [default: /fact/raw]
-    --qsub            use qsub for submission
-"""
-import os
-from docopt import docopt
-from os import remove
-from os.path import exists
-from os.path import dirname
 from os.path import join
-from os.path import abspath
+
 from fact.path import tree_path
 from functools import partial
-from shutil import which
-import subprocess as sp
-from manure import production_main
+import manure
 
-from .fad_counter_readout import run_fad_counter_extraction
-import scoop
+input_dir = '/fact/raw'
+out_dir = '/gpfs0/fact/processing/public/fad_counter'
 
-
-def init_path_generators(input_dir, out_dir):
-    return {
+manure.production_main(
+    path_generators={
         'input_file_path': partial(
             tree_path,
             prefix=input_dir,
@@ -43,75 +27,13 @@ def init_path_generators(input_dir, out_dir):
                 tree_path,
                 prefix=join(out_dir, 'fad_counters'),
                 suffix='_fad.h5'),
-    }
-
-
-def scoop_submit(job, list_of_futures):
-    list_of_futures.append(
-        scoop.futures.submit(
-            run_fad_counter_extraction,
-            {
-                'in_path': job.input_file_path,
-                'out_path': job.output_file_path,
-            }
-        )
-    )
-
-
-def qsub(job, queue='fact_medium'):
-
-    for p in [
-        job.std_out_path,
-        job.std_err_path,
-    ]:
-        if exists(p):
-            remove(p)
-        else:
-            os.makedirs(dirname(p), exist_ok=True)
-
-    cmd = [
-        'qsub',
-        '-q', queue,
-        '-o', job.std_out_path,
-        '-e', job.std_err_path,
-        which('single_fad_counter_extraction'),
-        job.input_file_path,
-        job.output_file_path,
-    ]
-
-    try:
-        sp.check_output(cmd, stderr=sp.STDOUT)
-    except sp.CalledProcessError as e:
-        print('returncode', e.returncode)
-        print('output', e.output)
-        raise
-
-
-def main():
-    args = docopt(__doc__)
-    out_dir = abspath(args['--output'])
-    input_dir = abspath(args['--input'])
-    path_generators = init_path_generators(input_dir, out_dir)
-
-    if args['--qsub']:
-        production_main(
-            path_generators,
-            qsub,
-            out_dir
-        )
-    else:
-        list_of_futures = []
-        scoop_submit_job = partial(
-            scoop_submit,
-            list_of_futures=list_of_futures
-        )
-        production_main(
-            path_generators,
-            scoop_submit_job,
-            out_dir,
-        )
-        scoop.futures.wait(list_of_futures)
-
-
-if __name__ == '__main__':
-    main()
+    },
+    submission=partial(
+        manure.qsub,
+        o_path='std_out_path',
+        e_path='std_err_path',
+        binary_name='single_fad_counter_extraction',
+        args=['input_file_path', 'output_file_path']
+    ),
+    runstatus_path=join(out_dir, 'runstatus.csv')
+)
